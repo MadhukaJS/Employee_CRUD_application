d31ea72a911a87ca67f493209b65cdc67a233f8f
Madhuka Priyashan <111411737+MadhukaJS@users.noreply.github.com>
2023-05-18 13:03:13 +0530

    main ceated

--- EmployeeCRUD/src/Employee.java
+++ EmployeeCRUD/src/Employee.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Employee {
     private JPanel main;
@@ -11,4 +13,21 @@ public class Employee {
     private JButton deleteButton;
     private JButton searchButton;
     private JTextField txtId;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Employee");
+        frame.setContentPane(new Employee().main);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public Employee() {
+    saveButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    });
+}
 }
