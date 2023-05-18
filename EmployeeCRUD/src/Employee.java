89f5971f88dc2214438207049a3e23dc368fddaa
Madhuka Priyashan <111411737+MadhukaJS@users.noreply.github.com>
2023-05-18 13:00:35 +0530

    UI created.

---
+++ EmployeeCRUD/src/Employee.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Employee {
+    private JPanel main;
+    private JTextField txtName;
+    private JTextField txtSallary;
+    private JTextField txtMobile;
+    private JButton saveButton;
+    private JTable table1;
+    private JButton updateButton;
+    private JButton deleteButton;
+    private JButton searchButton;
+    private JTextField txtId;
+}
